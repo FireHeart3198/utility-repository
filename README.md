70efd075326ef242e819f634ff9f8ca1be600ee5
mb <115391907+FireHeart3198@users.noreply.github.com>
2024-05-27 17:02:50 +1000

    Add drop-down-menu

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# todo-list
+# utility-repository
+
+A collection of small utility code to be reused
--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import "normalize.css";
 import "./style.css";
+import { targetDropDowns } from "./drop-down-menu";
+
+//drop-down-menu
+const elsWithDropdowns = document.querySelectorAll(".drop-down-menu button");
+elsWithDropdowns.forEach((el) => {
+  targetDropDowns(el);
+});
